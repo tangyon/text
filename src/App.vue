1712a5d200ec674c33ea9ee809472936ac0e205f
ty <2546922538@qq.com>
2020-05-08 13:38:28 +0800

    打印

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,50 @@
 <template>
-  <div id="app">
-    	<div class="audio">
-    		<audio controls autoplay preload >
-		   		<source  >
-				</audio>
-    	</div>
-		 <router-view />
-  </div>
+    <div id="app">
+        <div class="audio">
+            <audio controls autoplay preload>
+                <source>
+            </audio>
+        </div>
+        <router-view/>
+    </div>
 </template>
 
-<script >
-				
-		export default{
-					name:"App",
-					
-	}
-				
+<script>
+
+    export default {
+        name: "App",
+        mounted() {
+            console.log('================')
+        }
+
+    }
+
 </script>
 
 
 <style lang="less">
-	
-	html,body{
-		width: 100%;
- 		height: 100%;
-	}
-	
-#app {
- width: 100%;
- height: 100%;
- margin: 0;
- padding: 0;
-}
-.audio{
-			width: 0;
-			height: 0;
-			overflow: hidden;
-}
-audio{
-			width: 0;
-			height: 0;
-		}
-	
+
+    html, body {
+        width: 100%;
+        height: 100%;
+    }
+
+    #app {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    .audio {
+        width: 0;
+        height: 0;
+        overflow: hidden;
+    }
+
+    audio {
+        width: 0;
+        height: 0;
+    }
+
 </style>
